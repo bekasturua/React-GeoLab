1a864876c6bf0f15879850cdbb62b1137313b71e
Beka Sturua <beqasturua3@gmail.com>
2022-06-29 00:12:13 +0400

    add setInterval

--- src/Counter.js
+++ src/Counter.js
@@ -1,8 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "./Button";
 
 export default function Counter() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setCount((current) => current + 1);
+    }, 1000);
+  }, []);
 
   return (
     <div className="App">
