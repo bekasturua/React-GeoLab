12d15c9cbf661559039ad3940ebe41fbda2c8cff
Beka Sturua <beqasturua3@gmail.com>
2022-06-28 22:35:21 +0400

    add useEffect

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import "./App.css";
 import Button from "./Button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 let buttons = ["btn 1", "btn 2", "btn 3", "btn 4"];
 
 function App() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("test");
+
+  useEffect(() => {
+    fetch("https://google.com");
+  }, [name]);
 
   function onChangeClick(e) {
-    setCount(count + 1);
+    setCount((current) => {
+      return current + 1;
+    });
   }
 
   return (
     <div className="App">
       Current value: {count}
-      <Button onClick={onChangeClick} title="Change"></Button>
+      <Button onClick={onChangeClick} title="update count"></Button>
+      <Button onClick={() => setName("test 123")} title="update name"></Button>
     </div>
   );
 }
